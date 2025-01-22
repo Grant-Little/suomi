d537335989a47cc5d78d54575c887b6e572bef95
Grant Little <grantrl2001@gmail.com>
2025-01-22 18:23:45 -0500

    added file for memmem implementation

---
+++ suomi_internal.c
@@ -0,0 +1,1 @@
+#include "suomi_internal.h"
---
+++ suomi_internal.h
@@ -0,0 +1,8 @@
+#ifndef SUOMI_INTERNAL_H
+#define SUOMI_INTERNAL_H
+
+#include <stddef.h>
+
+void *smMemMem(const void *haystack, size_t haystack_length, const void *needle, size_t needle_length);
+
+#endif
