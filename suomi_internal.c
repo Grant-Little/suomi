a66b94e8ea3c53e99cf631bb0f799f70601d9d3e
Grant Little <grantrl2001@gmail.com>
2025-01-30 15:27:53 -0500

    i hate linked lists

--- suomi_internal.c
+++ suomi_internal.c
@@ -2,7 +2,7 @@
 
 bool smIsMemZeroed(const void *mem, size_t num_bytes) {
     for (size_t i = 0; i < num_bytes; i++) {
-        if ((unsigned char *)mem)[i] != (unsigned char)0 {
+        if (((unsigned char *)mem)[i] != (unsigned char)0) {
             return false;
         }
     }
