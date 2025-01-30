08b87f2dd555d57c4021457082b961a6ebd63366
Grant Little <grantrl2001@gmail.com>
2025-01-30 16:46:38 -0500

    removed macros file

--- suomi_macros.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUOMI_MACROS_H
-#define SUOMI_MACROS_H
-
-// use to generate hash table functions of fixed type keys and values, keys cannot by string literals because i don't want to conditionally include string.h, could be a fixed size char buffer though
-#define SM_GEN_HASH_TABLE_INTERFACE(interface_prefix, key_type, value_type)
-
-#endif
