20bfa97d432ce0dc5a1ed1f90a2c41c6460c3030
Grant Little <grantrl2001@gmail.com>
2025-02-20 11:56:15 -0500

    all my homies also hate strings

--- suomi.h
+++ suomi.h
@@ -27,50 +27,6 @@ void *smArenaPush(smError *error, smArena *arena, size_t num_bytes);
 void smArenaPop(smArena *arena, size_t num_bytes);
 
 typedef struct {
-    char *contents;
-    size_t length;
-    size_t capacity;
-} smString;
-
-// displaying strings
-void smStringPuts(const smString *string);
-
-// creating and destroying strings
-smString smStringInit(smError *error, smArena *arena, size_t capacity);
-smString smStringInitWithContents(smError *error, smArena *arena, const char *contents, size_t capacity);
-void smStringDeinit(smString *string);
-void smStringClear(smString *string);
-
-// comparing string contents
-// if you want to directly compare string structs, then do:
-// memcmp(&string1, &string2, sizeof(smString))
-bool smStringAreContentsSame(const smString *string1, const smString *string2);
-
-// index string
-char smStringIndex(smError *error, const smString *string, size_t index); //positive indices, return '\0' on invalid index
-
-// modifying strings
-void smStringAppendCstring(smError *error, smString *dest_string, const char *cstring);
-void smStringAppendString(smError *error, smString *dest_string, const smString *source_string);
-void smStringAppendSubString(smError *error, smString *dest_string, const smString *sub_string, size_t sub_string_index, size_t sub_string_length);
-void smStringCopyCstring(smError *error, smString *dest_string , const char *cstring);
-void smStringCopyString(smError *error, smString *dest_string, const smString *copied_string);
-void smStringCopySubString(smError *error, smString *dest_string, const smString *sub_string, size_t sub_string_index, size_t sub_string_length);
-void smStringWriteCstringAtIndex(smError *error, smString *dest_string, size_t dest_index, const char *cstring);
-void smStringWriteStringAtIndex(smError *error, smString *dest_string, size_t dest_index, const smString *source_string);
-void smStringWriteSubStringAtIndex(smError *error, smString *dest_string, size_t dest_index, const smString *sub_string, size_t sub_string_index, size_t sub_string_length);
-void smStringRemove(smError *error, smString *string, size_t index, size_t length);
-void smStringPush(smError *error, smString *string, char character);
-char smStringPop(smString *string);
-
-// search for first occurence, provide index and length to search, return SIZE_MAX if not found, 0 search_length is taken as searched_string->length
-// uses memmem which is not widely supported
-// potentially write own implementation of memmem to use if memmem not defined
-size_t smStringFindCstring(const smString *searched_string, size_t search_index, size_t search_length, const char *cstring);
-size_t smStringFindString(const smString *searched_string, size_t search_index, size_t search_length, const smString *find_string);
-size_t smStringFindSubString(smError *error, const smString *searched_string, size_t search_index, size_t search_length, const smString *sub_string, size_t sub_string_index, size_t sub_string_length);
-
-typedef struct {
     uintptr_t buckets;
     size_t bucket_num_bytes;
     uint32_t max_num_buckets;
